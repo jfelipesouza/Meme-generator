8947bf853c547ccb93f1e990e3468819c981496b
Felipe Souza <j.felipe.dev.18@gmail.com>
2021-10-26 18:31:05 -0300

    Iniciando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+function App() {
+  return (
+    <div>
+        Qualquer coisa
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Container,
+} from './styled'
+
+
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default Home;
